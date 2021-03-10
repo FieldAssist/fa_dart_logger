69fbc4b0d4c5eb95c6cd42ec04c208eb4fad3e1e
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-03-10 10:38:07 +0530

    [Null Safety] Migrate project to support null safety
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- lib/src/logger/output/my_console_output.dart
+++ lib/src/logger/output/my_console_output.dart
@@ -1,11 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 
 class MyConsoleOutput extends LogOutput {
+  MyConsoleOutput({this.packageName});
+
+  final String? packageName;
+
   @override
   void output(OutputEvent event) {
     event.lines.forEach((value) {
-      debugPrint(value);
+      if (packageName != null) {
+        assert(() {
+          print('${'${DateTime.now()}: $packageName: '}$value');
+          return true;
+        }(), '--');
+      } else {
+        assert(() {
+          print('${'${DateTime.now()}: '}$value');
+          return true;
+        }(), '--');
+      }
     });
   }
 }
